df8c1df83e4a52a67b6892773da54d84571b37cd
László Csatlós <plutoz01@gmail.com>
2018-12-26 10:41:04 +0100

    Additional test data

--- vehicle-depot-ms/src/main/resources/db/migration/V1__Schema_init.sql
+++ vehicle-depot-ms/src/main/resources/db/migration/V1__Schema_init.sql
@@ -26,8 +26,12 @@ ALTER TABLE vehicle
 INSERT INTO depot(id, name) VALUES
 (1, 'Depot 1'),
 (2, 'Depot 2'),
-(3, 'Depot 3');
-SELECT setval('depot_id_seq', 4, true);
+(3, 'Depot 3'),
+(4, 'Depot 4'),
+(5, 'Depot 5'),
+(6, 'Depot 6'),
+(7, 'Depot 7');
+SELECT setval('depot_id_seq', 8, true);
 
 INSERT INTO vehicle(id, depot_id, licence_plate) VALUES
 (1, 1, 'D1-001'),
@@ -38,8 +42,11 @@ INSERT INTO vehicle(id, depot_id, licence_plate) VALUES
 (6, 3, 'D3-001'),
 (7, 3, 'D3-002'),
 (8, 3, 'D3-003'),
-(9, 3, 'D3-004');
-SELECT setval('vehicle_id_seq', 10, true);
+(9, 3, 'D3-004'),
+(10, 3, 'D3-005'),
+(11, 3, 'D3-006');
+
+SELECT setval('vehicle_id_seq', 12, true);
 
 
 

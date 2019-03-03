a3ec9704d7d63238962c30f45c464ccd582f22cc
László Csatlós <plutoz01@gmail.com>
2019-03-03 08:14:16 +0100

    Gradle fix

--- backend/src/main/java/com/plutoz/carryit/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.plutoz.carryit.repository;
-
-import com.plutoz.carryit.domain.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
